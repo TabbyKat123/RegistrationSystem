a3e0c3baef1691ee299a93945051775d03f7937e
Brenda Tabitha Namwone <51475285+TabbyKat123@users.noreply.github.com>
2021-01-29 14:59:38 +0300

    Added new files to my registration system folder
    I didn't add the venv files.

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, flash, redirect, url_for, session, logging
+from flask_mysqldb import MYSQL, MySQLdb
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators, request
+from passlib.hash import sha256_crypt
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+class RegisterForm(Form):
+    name = StringField('userame',[validators.Length(min=1, max = 50)])
+    id = StringField('id', [validators.Length(min=4, max=5)])
+    email = StringField('Email', [validators.Length(min=6, max=9)])
+    gender = StringField('Gender', [validators.Length(min=1, max=3)])
+    course = StringField('Course',[validators.Length(min=3, max=6)])
+    dob = StringField('Date Of Birth',[validators.Length(min=5, max=6)])
+    nationality = StringField('Nationality', [validators.Length(min=8, max=13)])
+    password = PasswordField('Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message = 'Passwords do not match')
+    ])
+    confirm = PasswordField('Confirm Passowrd')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm(request.form)
+    if request.method =='POST' and form.validate():
+        return render_template('register.html')
+    return render_template('register.html', form = form)
+
+
+
+if __name__ == '__main__':
+    app.run(debug = True)
